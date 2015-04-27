d176b17236b8b46f239d94b9d044afaedd193466
CyberCastle <CyberCastle@gmail.com>
2015-04-27 10:15:53 -0300

    Se habilita la visualización del video, para el desarrollo de la aplicación en un desktop

--- OpenCV-Raspberry/src/main/java/cl/cc/main/Run.java
+++ OpenCV-Raspberry/src/main/java/cl/cc/main/Run.java
@@ -33,15 +33,15 @@ public class Run {
         System.out.println("Initializing Camera ...");
         FaceDetector face = new FaceDetector();
 
-//        // Frame setting
-//        JFrame frame = new JFrame("CyberCastle Says: ");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(400, 400);
-//
-//        // Image viewer setting
-//        OpenCVMatPanel cvPanel = new OpenCVMatPanel();
-//        frame.setContentPane(cvPanel);
-//        frame.setVisible(true);
+        // Frame setting
+        JFrame frame = new JFrame("CyberCastle Says: ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        // Image viewer setting
+        OpenCVMatPanel cvPanel = new OpenCVMatPanel();
+        frame.setContentPane(cvPanel);
+        frame.setVisible(true);
 
         // Read the video stream 
         Mat cameraImage = new Mat();
@@ -65,35 +65,35 @@ public class Run {
                     System.out.println("Faces detected: " + face.getFaceRegions().length);
 
                     // Display the image
-//                    frame.setSize(i.width() + 40, i.height() + 60);
-//                    if (face.getFaceRegions().length > 0) {
-//                        cvPanel.drawMat(face.createMask(0, i));
-//                    } else {
-//                        //cvPanel.drawString("No hay Imagen....");
-//                        cvPanel.drawMat(processor.result());
-//                    }
-//
-//                    // Repaint the viewer
-//                    cvPanel.repaint();
+                    frame.setSize(i.width() + 40, i.height() + 60);
+                    if (face.getFaceRegions().length > 0) {
+                        cvPanel.drawMat(face.createMask(0, i));
+                    } else {
+                        //cvPanel.drawString("No hay Imagen....");
+                        cvPanel.drawMat(processor.result());
+                    }
+
+                    // Repaint the viewer
+                    cvPanel.repaint();
                 } else {
                     // Not image, exiting...
                     break;
                 }
 
                 // Routine for exit.....
-                if (readString.equals("\\q")) {
-                    readString = null;
-                    System.out.println("Exiting ... ");
-                } else if (scanner.hasNextLine()) {
-                    readString = scanner.nextLine();
-                }
+//                if (readString.equals("\\q")) {
+//                    readString = null;
+//                    System.out.println("Exiting ... ");
+//                } else if (scanner.hasNextLine()) {
+//                    readString = scanner.nextLine();
+//                }
             }
         } else {
              System.out.println("Camera not initialized.");
         }
     }
 
-    // Método para agregar runtime una librería nativa.
+    // Método para agregar en tiempo de ejecución una librería nativa.
     // Pieza de código obtenida desde aquí: http://fahdshariff.blogspot.com/2011/08/changing-java-library-path-at-runtime.html
     private static void addLibraryPath(String libraryPath) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
         String newLibraryPath = libraryPath + ":" + System.getProperty("java.library.path");
